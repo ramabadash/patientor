80c7cff16c42dc7c393ba5eab97f4db06e4531ea
ramabadash <89576646+ramabadash@users.noreply.github.com>
2021-12-11 00:17:50 +0200

    prettier code style

--- src/state/state.tsx
+++ src/state/state.tsx
@@ -1,35 +1,25 @@
-import React, { createContext, useContext, useReducer } from "react";
-import { Patient } from "../types";
+import React, { createContext, useContext, useReducer } from 'react';
+import { Patient } from '../types';
 
-import { Action } from "./reducer";
+import { Action } from './reducer';
 
 export type State = {
   patients: { [id: string]: Patient };
 };
 
 const initialState: State = {
-  patients: {}
+  patients: {},
 };
 
-export const StateContext = createContext<[State, React.Dispatch<Action>]>([
-  initialState,
-  () => initialState
-]);
+export const StateContext = createContext<[State, React.Dispatch<Action>]>([initialState, () => initialState]);
 
 type StateProviderProps = {
   reducer: React.Reducer<State, Action>;
   children: React.ReactElement;
 };
 
-export const StateProvider: React.FC<StateProviderProps> = ({
-  reducer,
-  children
-}: StateProviderProps) => {
+export const StateProvider: React.FC<StateProviderProps> = ({ reducer, children }: StateProviderProps) => {
   const [state, dispatch] = useReducer(reducer, initialState);
-  return (
-    <StateContext.Provider value={[state, dispatch]}>
-      {children}
-    </StateContext.Provider>
-  );
+  return <StateContext.Provider value={[state, dispatch]}>{children}</StateContext.Provider>;
 };
 export const useStateValue = () => useContext(StateContext);
